d817a11e39c96a6882a606b0a9c418067a1dd832
Tobias Hafner <mail@tobiashafner.ch>
2024-05-30 19:11:00 +0200

    Add native type superclass for custom type objects

---
+++ include/NativeType.h
@@ -0,0 +1,17 @@
+//
+// Created by tobi on 30.05.24.
+//
+
+#ifndef POLYPHENY_CPP_DRIVER_NATIVETYPE_H
+#define POLYPHENY_CPP_DRIVER_NATIVETYPE_H
+
+namespace Types {
+
+    class NativeType {
+    public:
+        virtual ~NativeType() = default;
+    };
+
+} // Types
+
+#endif //POLYPHENY_CPP_DRIVER_NATIVETYPE_H
