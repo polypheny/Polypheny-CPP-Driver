9ff26a7fb2fca8ced4f8804f5a78a3b153905f38
Tobias Hafner <mail@tobiashafner.ch>
2024-05-30 16:38:15 +0200

    Add common result class

---
+++ include/Result.h
@@ -0,0 +1,47 @@
+//
+// Created by tobi on 30.05.24.
+//
+
+#ifndef POLYPHENY_CPP_DRIVER_RESULT_H
+#define POLYPHENY_CPP_DRIVER_RESULT_H
+
+#include <stdexcept>
+#include <string>
+#include <typeinfo>
+
+namespace Results {
+
+    enum class ResultType {
+        RELATIONAL,
+        DOCUMENT,
+        GRAPH,
+        SCALAR
+    };
+
+    class Result {
+    private:
+        ResultType resultType;
+
+        Result(ResultType resultType) : resultType(resultType) {}
+
+        virtual ~Result() = default;
+
+        ResultType getResultType() const {
+            return resultType;
+        }
+
+        template<typename T>
+        T *unwrap() {
+            T *derived = dynamic_cast<T *>(this);
+            if (derived) {
+                return derived;
+            } else {
+                throw std::runtime_error("Not a wrapper for " + std::string(typeid(T).name()));
+            }
+        }
+
+    };
+
+} // Results
+
+#endif //POLYPHENY_CPP_DRIVER_RESULT_H
---
+++ src/Result.cpp
@@ -0,0 +1,8 @@
+//
+// Created by tobi on 30.05.24.
+//
+
+#include "Result.h"
+
+namespace Results {
+} // Results
