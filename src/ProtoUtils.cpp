d3867030ce01cb020d29c9afcb7ac6e1d6f8e440
Tobias Hafner <mail@tobiashafner.ch>
2024-05-31 22:09:21 +0200

    Use unique pointers for passed messages

--- src/ProtoUtils.cpp
+++ src/ProtoUtils.cpp
@@ -29,41 +29,46 @@ namespace Utils {
         return std::chrono::milliseconds(time.time());
     }
 
-    std::list<TypedValue> ProtoUtils::proto_to_list(const org::polypheny::prism::ProtoList &list) {
+    std::list<Types::TypedValue> ProtoUtils::proto_to_list(const org::polypheny::prism::ProtoList &list) {
         google::protobuf::RepeatedPtrField<::org::polypheny::prism::ProtoValue> values = list.values();
-        std::list<TypedValue> result;
+        std::list<Types::TypedValue> result;
         for (const org::polypheny::prism::ProtoValue &value: values) {
             result.emplace_back(value);
         }
         return result;
     }
 
-    org::polypheny::prism::ProtoTimestamp
-    ProtoUtils::timestamp_to_proto(const std::chrono::system_clock::time_point &timestamp) {
-        org::polypheny::prism::ProtoTimestamp proto_timestamp;
-        proto_timestamp.set_timestamp(
+    std::unique_ptr<org::polypheny::prism::ProtoTimestamp>
+    Utils::ProtoUtils::timestamp_to_proto(const std::chrono::system_clock::time_point &timestamp) {
+        auto proto_timestamp = std::make_unique<org::polypheny::prism::ProtoTimestamp>();
+        proto_timestamp->set_timestamp(
                 std::chrono::duration_cast<std::chrono::milliseconds>(timestamp.time_since_epoch()).count());
         return proto_timestamp;
     }
 
-    org::polypheny::prism::ProtoDate ProtoUtils::date_to_proto(const std::chrono::system_clock::time_point &date) {
-        org::polypheny::prism::ProtoDate proto_date;
+    std::unique_ptr<org::polypheny::prism::ProtoDate>
+    Utils::ProtoUtils::date_to_proto(const std::chrono::system_clock::time_point &date) {
+        auto proto_date = std::make_unique<org::polypheny::prism::ProtoDate>();
         uint64_t millis_since_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(
                 date.time_since_epoch()).count();
-        proto_date.set_date(millis_since_epoch / MILLIS_PER_DAY);
+        proto_date->set_date(millis_since_epoch / MILLIS_PER_DAY);
         return proto_date;
     }
 
-    org::polypheny::prism::ProtoTime ProtoUtils::time_to_proto(const std::chrono::milliseconds &time) {
-        org::polypheny::prism::ProtoTime proto_time;
-        proto_time.set_time(time.count());
+
+    std::unique_ptr<org::polypheny::prism::ProtoTime>
+    Utils::ProtoUtils::time_to_proto(const std::chrono::milliseconds &time) {
+        auto proto_time = std::make_unique<org::polypheny::prism::ProtoTime>();
+        proto_time->set_time(time.count());
         return proto_time;
     }
 
-    org::polypheny::prism::ProtoList ProtoUtils::list_to_proto(const std::list<TypedValue> &typed_values) {
-        org::polypheny::prism::ProtoList proto_list;
+
+    std::unique_ptr<org::polypheny::prism::ProtoList>
+    Utils::ProtoUtils::list_to_proto(const std::list<Types::TypedValue> &typed_values) {
+        auto proto_list = std::make_unique<org::polypheny::prism::ProtoList>();
         for (const Types::TypedValue &typed_value: typed_values) {
-            *proto_list.add_values() = typed_value.serialize();
+            *proto_list->add_values() = *typed_value.serialize(); // Assuming serialize returns a unique_ptr
         }
         return proto_list;
     }
