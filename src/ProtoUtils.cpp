eaf0986f71bdd9fe593843f88aef542525e0d3c0
Tobias Hafner <mail@tobiashafner.ch>
2024-05-31 15:36:56 +0200

    Refactor to use braced initializers

--- src/ProtoUtils.cpp
+++ src/ProtoUtils.cpp
@@ -6,10 +6,10 @@
 
 namespace Utils {
     std::vector<uint8_t> ProtoUtils::string_to_vector(const std::string &string) {
-        return std::vector<uint8_t>(input.begin(), input.end());
+        return {string.begin(), string.end()};
     }
 
     std::string ProtoUtils::vector_to_string(const std::vector<uint8_t> &vector) {
-        return std::string(input.begin(), input.end());
+        return {vector.begin(), vector.end()};
     }
 }
