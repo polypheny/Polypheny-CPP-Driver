c3188a2b77755f5f09d265aa50478396ae0d6d02
Tobias Hafner <mail@tobiashafner.ch>
2024-06-05 20:53:28 +0200

    Prepare folder structure for example executables

---
+++ include/PolyphenyCPPDriver.h
@@ -0,0 +1,29 @@
+//
+// Created by tobi on 05.06.24.
+//
+
+#ifndef POLYPHENY_CPP_DRIVER_POLYPHENYCPPDRIVER_H
+#define POLYPHENY_CPP_DRIVER_POLYPHENYCPPDRIVER_H
+
+// connection
+#include "Connection.h"
+#include "ConnectionProperties.h"
+#include "Cursor.h"
+
+// results
+#include "Result.h"
+#include "ScalarResult.h"
+#include "RelationalResult.h"
+#include "DocumentResult.h"
+#include "GraphResult.h"
+
+// native types
+#include "NativeType.h"
+#include "BigDecimal.h"
+#include "Document.h"
+#include "Interval.h"
+
+// values
+#include "TypedValue.h"
+
+#endif //POLYPHENY_CPP_DRIVER_POLYPHENYCPPDRIVER_H
