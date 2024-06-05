ab15b5fdd2dfee03ccd6f8ff4b1da9bb5656120d
Tobias Hafner <mail@tobiashafner.ch>
2024-06-05 21:15:40 +0200

    Add connection examples

--- examples/ExampleConnectionDefaults.cpp
+++ examples/ExampleConnectionDefaults.cpp
@@ -1,6 +1,24 @@
 #include "PolyphenyCPPDriver.h"
+#include <memory>
 
 int main() {
+    // valid connection parameters to connect to a polypheny instance
+    std::string host = "localhost";
+    std::string username = "pa";
+    std::string password = "pa";
 
+    // open a connection to the polypheny instance
+    Connection::Connection database_connection = Connection::Connection(host, username, password);
+
+    // get a cursor operating on the previously opened connection
+    Connection::Cursor cursor(database_connection);
+
+    // execute a statement using this cursor
+    std::unique_ptr<Results::Result> result = cursor.execute("sql", "SELECT * FROM emps;", "public");
+
+    // the result can now be processed
+    // for further details please refer to the examples "ExampleRelational", "ExampleDocument" and "ExampleGraph"
+
+    // TODO: close connection
 
 }
--- examples/ExampleConnectionProperties.cpp
+++ examples/ExampleConnectionProperties.cpp
@@ -1,3 +1,31 @@
-//
-// Created by tobi on 05.06.24.
-//
+#include "PolyphenyCPPDriver.h"
+#include <memory>
+
+int main() {
+    // for more control over the configuration of the connection a connection properties object can be created
+    // this object is initialized with the default values
+    Connection::ConnectionProperties properties = Connection::ConnectionProperties();
+
+    // we can now customize these values
+    properties.set_username("custom_user");
+    properties.set_password("strong_password");
+    properties.set_host("127.0.0.1");
+    properties.set_port(7987);
+    properties.set_is_auto_commit(false);
+    properties.set_default_namespace("custom_namespace");
+
+    // the connection properties can now be used to open arbitrarily many connections
+    Connection::Connection database_connection(properties);
+
+    // get a cursor operating on the previously opened connection
+    Connection::Cursor cursor(database_connection);
+
+    // execute a statement using this cursor
+    std::unique_ptr<Results::Result> result = cursor.execute("sql", "SELECT * FROM emps;", "public");
+
+    // the result can now be processed
+    // for further details please refer to the examples "ExampleRelational", "ExampleDocument" and "ExampleGraph"
+
+    // TODO: close connection
+
+}
