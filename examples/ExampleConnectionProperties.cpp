9b49fb380f2461cf950e5e3af36a970c79ef90b2
Tobias Hafner <mail@tobiashafner.ch>
2024-06-06 18:01:19 +0200

    Update connection properties in example

--- examples/ExampleConnectionProperties.cpp
+++ examples/ExampleConnectionProperties.cpp
@@ -7,14 +7,14 @@ int main() {
     Connection::ConnectionProperties properties = Connection::ConnectionProperties();
 
     // we can now customize these values
-    properties.set_username("custom_user");
-    properties.set_password("strong_password");
-    properties.set_host("127.0.0.1");
-    properties.set_port(7987);
-    properties.set_is_auto_commit(false);
-    properties.set_default_namespace("custom_namespace");
+    properties.set_username("pa");
+    properties.set_password("");
+    properties.set_host("localhost");
+    properties.set_port(20590);
+    properties.set_is_auto_commit(true);
+    properties.set_default_namespace("public");
 
-    // the connection properties can now be used to open arbitrarily many connections
+    // the connection properties can now be used to open connections
     Connection::Connection database_connection(properties);
 
     // get a cursor operating on the previously opened connection
@@ -25,7 +25,7 @@ int main() {
 
     // the result can now be processed
     // for further details please refer to the examples "ExampleRelational", "ExampleDocument" and "ExampleGraph"
+    std::cout << "Example completed successfully";
 
-    // TODO: close connection
-
+    // connection is automatically closed in destructor of the connection
 }
