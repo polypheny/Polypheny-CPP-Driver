297d150bc2299d91da00328ed042c628f4533f86
Tobias Hafner <mail@tobiashafner.ch>
2024-06-06 11:13:01 +0200

    Add skeletons for examples

--- src/TransportUtils.h
+++ src/TransportUtils.h
@@ -5,6 +5,8 @@
 #ifndef POLYPHENY_CPP_DRIVER_TRANSPORTUTILS_H
 #define POLYPHENY_CPP_DRIVER_TRANSPORTUTILS_H
 
+#include <cstdint>
+
 namespace Utils::TransportUtils {
         uint64_t reverse_byte_order(uint64_t value);
     }
