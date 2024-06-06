1b5ba858f86c0d4eea96776cf6ddef7027ac7819
Tobias Hafner <mail@tobiashafner.ch>
2024-06-06 13:17:50 +0200

    Improve error messages

--- src/PlainTransport.cpp
+++ src/PlainTransport.cpp
@@ -14,24 +14,24 @@ namespace Communication {
         hints.ai_family = AF_UNSPEC;
         hints.ai_socktype = SOCK_STREAM;
         if (getaddrinfo(host.c_str(), std::to_string(port).c_str(), &hints, &res) != 0) {
-            throw std::runtime_error("getaddrinfo failed");
+            throw std::runtime_error("getaddrinfo failed: " + std::string(strerror(errno)));
         }
         socket_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
         if (socket_fd == -1) {
             freeaddrinfo(res);
-            throw std::runtime_error("Failed to create socket");
+            throw std::runtime_error("Failed to create socket: " + std::string(strerror(errno)));
         }
         if (connect(socket_fd, res->ai_addr, res->ai_addrlen) == -1) {
             freeaddrinfo(res);
             close_socket();
-            throw std::runtime_error("Failed to connect to server");
+            throw std::runtime_error("Failed to connect to server: " + std::string(strerror(errno)));
         }
         freeaddrinfo(res);
 
         int flag = 1;
         if (setsockopt(socket_fd, IPPROTO_TCP, TCP_NODELAY, (char *) &flag, sizeof(int)) < 0) {
             close_socket();
-            throw std::runtime_error("Failed to set TCP_NODELAY");
+            throw std::runtime_error("Failed to set TCP_NODELAY: " + std::string(strerror(errno)));
         }
 
         exchange_version();
