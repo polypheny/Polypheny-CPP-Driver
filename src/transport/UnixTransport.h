03755bed7cab6ea6e1fc730f36a6136962440126
Tobias Hafner <mail@tobiashafner.ch>
2024-06-07 10:24:56 +0200

    Add unix transport

---
+++ src/transport/UnixTransport.h
@@ -0,0 +1,40 @@
+//
+// Created by tobi on 07.06.24.
+//
+
+#ifndef POLYPHENY_CPP_DRIVER_UNIXTRANSPORT_H
+#define POLYPHENY_CPP_DRIVER_UNIXTRANSPORT_H
+
+#include <string>
+#include <vector>
+#include <mutex>
+#include <stdexcept>
+#include <cstring>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include "PlainTransport.h"
+
+namespace Communication {
+
+    class UnixTransport {
+    public:
+        static const std::string VERSION;
+
+        UnixTransport(const std::string &path);
+        ~UnixTransport();
+
+        void send_message(const std::string &message);
+        std::string receive_message();
+
+    private:
+        int socket_fd;
+        std::mutex write_mutex;
+
+        void close_socket();
+        void exchange_version();
+    };
+
+} // Communication
+
+#endif //POLYPHENY_CPP_DRIVER_UNIXTRANSPORT_H
