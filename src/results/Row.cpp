c02a82d04442d69ade098cf728e446453eae3325
Tobias Hafner <mail@tobiashafner.ch>
2024-06-07 10:56:15 +0200

    Add row for relational results

--- src/results/Row.cpp
+++ src/results/Row.cpp
@@ -4,8 +4,53 @@
 
 #include "Row.h"
 
+#include <utility>
+
 namespace Results {
-    Row::Row(const org::polypheny::prism::Row row, std::shared_ptr<Results::RelationalMetadata> sharedPtr) {
+
+    Row::Row(const org::polypheny::prism::Row &row, std::shared_ptr<Results::RelationalMetadata> relational_metadata)
+            : relational_metadata(std::move(relational_metadata)) {
+        for (const auto& value : row.values()) {
+            values.emplace_back(value);
+        }
 
     }
+
+    Types::TypedValue &Row::operator[](size_t column_index) {
+        return values[column_index];
+    }
+
+    const Types::TypedValue &Row::operator[](size_t column_index) const {
+        return values[column_index];
+    }
+
+    Types::TypedValue &Row::operator[](const std::string &column_name) {
+        return values[relational_metadata->get_column_index_from_label(column_name)];
+    }
+
+    const Types::TypedValue &Row::operator[](const std::string &column_name) const {
+        return values[relational_metadata->get_column_index_from_label(column_name)];
+    }
+
+    Types::TypedValue Row::at(size_t column_index) {
+        return values.at(column_index);
+    }
+
+    Types::TypedValue Row::at(const std::string &column_name) {
+        return values.at(relational_metadata->get_column_index_from_label(column_name));
+    }
+
+    Types::TypedValue Row::at(size_t column_index) const {
+        return values.at(column_index);
+    }
+
+    Types::TypedValue Row::at(const std::string &column_name) const {
+        return values.at(relational_metadata->get_column_index_from_label(column_name));
+    }
+
+    size_t Row::size() const {
+        return values.size();
+    }
+
+
 } // Results
--- src/results/Row.h
+++ src/results/Row.h
@@ -1,21 +1,43 @@
-//
-// Created by tobi on 06.06.24.
-//
-
 #ifndef POLYPHENY_CPP_DRIVER_ROW_H
 #define POLYPHENY_CPP_DRIVER_ROW_H
 
+#include <vector>
+#include <memory>
+#include <string>
 #include "relational_frame.pb.h"
 #include "RelationalMetadata.h"
+#include "types/TypedValue.h"
 
 namespace Results {
 
     class Row {
 
     public:
-        Row(const org::polypheny::prism::Row row, std::shared_ptr<Results::RelationalMetadata> sharedPtr);
+        Row(const org::polypheny::prism::Row &row, std::shared_ptr<Results::RelationalMetadata> relational_metadata);
+
+        Types::TypedValue &operator[](size_t column_index);
+
+        const Types::TypedValue &operator[](size_t column_index) const;
+
+        Types::TypedValue &operator[](const std::string &column_name);
+
+        const Types::TypedValue &operator[](const std::string &column_name) const;
+
+        Types::TypedValue at(size_t column_index);
+
+        Types::TypedValue at(const std::string &column_name);
+
+        [[nodiscard]] Types::TypedValue at(size_t column_index) const;
+
+        [[nodiscard]] Types::TypedValue at(const std::string &column_name) const;
+
+        size_t size() const;
+
+    private:
+        std::vector<Types::TypedValue> values;
+        std::shared_ptr<Results::RelationalMetadata> relational_metadata;
     };
 
-} // Results
+} // namespace Results
 
-#endif //POLYPHENY_CPP_DRIVER_ROW_H
+#endif // POLYPHENY_CPP_DRIVER_ROW_H
