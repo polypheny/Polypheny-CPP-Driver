f7bf30f36d8be4cf06ae709bedd1218e3917688d
Tobias Hafner <mail@tobiashafner.ch>
2024-06-11 17:44:24 +0200

    Add relational result with example

--- src/results/DocumentResult.cpp
+++ src/results/DocumentResult.cpp
@@ -1,10 +1,72 @@
-//
-// Created by tobi on 01.06.24.
-//
-
 #include "DocumentResult.h"
 #include "src/connection/Cursor.h"
+#include "src/connection/Connection.h"
+#include "src/types/TypedValue.h"
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <stdexcept>
 
 namespace Results {
-    DocumentResult::DocumentResult(const org::polypheny::prism::DocumentFrame& document_frame, bool is_last, Connection::Cursor *cursor) : Result(ResultType::DOCUMENT) {}
+
+    DocumentResult::DocumentResult(const org::polypheny::prism::DocumentFrame &document_frame,
+                                   bool is_last, Connection::Cursor *cursor)
+            : Result(ResultType::DOCUMENT), is_fully_fetched(is_last), cursor(cursor) {
+        add_documents(document_frame);
+    }
+
+    void DocumentResult::add_documents(const org::polypheny::prism::DocumentFrame &documentFrame) {
+        for (const auto &proto_document : documentFrame.documents()) {
+            std::unordered_map<std::string, Types::TypedValue> document;
+            for (const auto &field : proto_document.entries()) {
+                document[field.key().string().string()] = Types::TypedValue(field.value());
+            }
+            documents.push_back(document);
+        }
+    }
+
+    void DocumentResult::fetch_more() {
+        uint32_t statement_id = cursor->get_statement_id();
+        org::polypheny::prism::Frame frame = cursor->get_connection().get_prism_interface_client().fetch_result(
+                statement_id, DEFAULT_FETCH_SIZE);
+
+        if (frame.result_case() != org::polypheny::prism::Frame::ResultCase::kDocumentFrame) {
+            throw std::runtime_error("Statement returned a result of illegal type.");
+        }
+
+        is_fully_fetched = frame.is_last();
+        add_documents(frame.document_frame());
+    }
+
+    DocumentResult::DocumentIterator DocumentResult::begin() {
+        return {std::shared_ptr<DocumentResult>(this, [](DocumentResult*){}), 0};
+    }
+
+    DocumentResult::DocumentIterator DocumentResult::end() {
+        return {std::shared_ptr<DocumentResult>(this, [](DocumentResult*){}), documents.size()};
+    }
+
+    DocumentResult::DocumentIterator::DocumentIterator(std::shared_ptr<DocumentResult> result, size_t index)
+            : result(std::move(result)), index(index) {}
+
+    bool DocumentResult::DocumentIterator::operator==(const DocumentIterator &other) const {
+        return index == other.index && result == other.result;
+    }
+
+    bool DocumentResult::DocumentIterator::operator!=(const DocumentIterator &other) const {
+        return !(*this == other);
+    }
+
+    std::unordered_map<std::string, Types::TypedValue>& DocumentResult::DocumentIterator::operator*() {
+        return *std::next(result->documents.begin(), index);
+    }
+
+    DocumentResult::DocumentIterator &DocumentResult::DocumentIterator::operator++() {
+        ++index;
+        if (index >= result->documents.size() && !result->is_fully_fetched) {
+            result->fetch_more();
+        }
+        return *this;
+    }
+
 } // Results
--- src/results/DocumentResult.h
+++ src/results/DocumentResult.h
@@ -7,6 +7,7 @@
 
 #include "Result.h"
 #include "document_frame.pb.h"
+#include "types/TypedValue.h"
 
 namespace Connection {
     class Cursor;
@@ -16,7 +17,32 @@ namespace Results {
 
     class DocumentResult : public Result {
     public:
-        explicit DocumentResult(const org::polypheny::prism::DocumentFrame &document_frame, bool is_lst, Connection::Cursor *cursor);
+        DocumentResult(const org::polypheny::prism::DocumentFrame &document_frame, bool is_last, Connection::Cursor *cursor);
+
+        class DocumentIterator {
+        public:
+            DocumentIterator(std::shared_ptr<DocumentResult> result, size_t index);
+
+            bool operator==(const DocumentIterator &other) const;
+            bool operator!=(const DocumentIterator &other) const;
+            std::unordered_map<std::string, Types::TypedValue>& operator*();
+            DocumentIterator &operator++();
+
+        private:
+            std::shared_ptr<DocumentResult> result;
+            size_t index;
+        };
+
+        DocumentIterator begin();
+        DocumentIterator end();
+
+    private:
+        void add_documents(const org::polypheny::prism::DocumentFrame &documentFrame);
+        void fetch_more();
+
+        std::vector<std::unordered_map<std::string, Types::TypedValue>> documents;
+        bool is_fully_fetched;
+        Connection::Cursor *cursor;
     };
 
 } // Results
