7408f8dfa0eca62c7536a19da2c060ee42a1e91e
Tobias Hafner <mail@tobiashafner.ch>
2024-08-09 11:53:31 +0200

    Add type to node constructor

--- src/results/Node.cpp
+++ src/results/Node.cpp
@@ -6,7 +6,7 @@
 
 namespace Results {
     Node::Node(org::polypheny::prism::ProtoNode proto_edge)
-            : GraphElement(proto_edge.id(), proto_edge.name()) {
+            : GraphElement(proto_edge.id(), proto_edge.name(), GraphElementType::NODE) {
         labels.assign(proto_edge.labels().begin(), proto_edge.labels().end());
         for (const auto &property: proto_edge.properties()) {
             emplace(property.key().string().string(), property.value());
